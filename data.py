021cf5e8878b8ec02f623744ba7830dc39d04504
Pranit Waydhane <35225694+pranitway@users.noreply.github.com>
2021-05-17 13:59:56 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,20 +1,5 @@
 import requests
 
-# parameters = {
-#     "amount" : 10,
-#     "type" : 'boolean',
-# }
-
-# try:
-#     response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean", params=parameters)
-# except Exception as e:
-#     print("First connect to the internet...")
-# else:
-#     response.raise_for_status()
-#     response.encoding = "utf-8"
-#     question_data = response.json()["results"]
-
-
 class Request_question():
     def __init__(self):
         self.parameters = {
